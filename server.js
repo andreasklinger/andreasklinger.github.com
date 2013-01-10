f18353a0e1f040048d99f6e6f46a53328b8ae594
Andreas Klinger <ak@garmz.com>
2013-01-10 10:37:30 +0100

    Better parameters

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var fs = require('fs'),
     http = require('http');
 
 
-var path = process.argv[2] || "/tumblr";
+var path = process.argv[2] || ".";
 if (path[0] != "/") path = "/" + path
 if (path[path.length -1] == "/") path = path.slice(0,-1)
 
